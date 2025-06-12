9db8b749cbeb816e36d7c9291b0ba6ca0337ee94
diogocarrer <diogocarrer14@gmail.com>
2025-06-12 10:21:15 -0300

    🔧 chore: projeto inicial gerado com Spring Boot

---
+++ src/main/java/com/equals/concilia/ConciliaApplication.java
@@ -0,0 +1,13 @@
+package com.equals.concilia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConciliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConciliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equals/concilia/ConciliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equals.concilia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConciliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
